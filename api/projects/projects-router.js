24c16606b420ff8863cbf1f3a81c65856fc17bc7
Matthew Hammel <mattyhammel17@gmail.com>
2022-02-11 15:28:34 -0700

    got most of everything done offline will fix things as interent stables out

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,55 @@
-// Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const Project = require('./projects-model');
+
+const { validateProjectId, validateProject } = require('./projects-middleware.js');
+
+router.get('/', (req, res) => {
+    Project.get()
+        .then(projects => {
+            res.json(projects);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "Error retrieving projects" });
+        })
+})
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.project);
+})
+
+router.post('/', validateProject, (req, res) => {
+        Project.insert({ name: req.name, description: req.description, completed: true })
+            .then(project => {
+                res.status(201).json(project);
+            })
+            .catch(() => {
+                res.status(500).json({ message: "Error creating project" });
+            })
+})
+
+router.put('/:id', validateProjectId, validateProject, (req, res, next) => {
+    Project.update(req.params.id, { name: req.name, description: req.description, completed: true })
+        .then(project => {
+            res.json(project);
+        })
+        .catch(next);
+
+})
+
+router.delete('/:id', validateProjectId, (req, res, next) => {
+    Project.remove(req.params.id)
+        .catch(next);
+})
+
+router.get('/:id/actions', (req, res) => {
+    Project.getProjectActions(req.params.id)
+        .then(actions => {
+            res.json(actions);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "Error retrieving project actions" });
+        })
+})
+
+module.exports = router;
